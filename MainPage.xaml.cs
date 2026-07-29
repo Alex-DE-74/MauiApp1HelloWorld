9fe6cc4722a7491bccfa254a4191344f794d8218
Alex-DE-74 <infalanda@gmail.com>
2026-07-29 22:36:11 +0200

    Remove TTS_SERVICE permission from MainPage

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -10,7 +10,6 @@ using Android.Content;
 
 // HIER ERGÄNZT: Der Compiler schreibt das nun absolut fehlerfrei ins Manifest!
 [assembly: UsesPermission(Android.Manifest.Permission.BodySensors)]
-[assembly: QueryIntentActions(new[] { "android.intent.action.TTS_SERVICE" })]
 #endif
 
 namespace MauiApp1HelloWorld;
