220f130fb82410306b830d35de26ebff594f3f60
Alex-DE-74 <infalanda@gmail.com>
2026-07-30 18:24:43 +0200

    Add AlarmReceiver to manage alarm notifications
    Implement AlarmReceiver to handle alarm events and start MainActivity.

---
+++ Platforms/Android/AlarmReceiver.cs
@@ -0,0 +1,25 @@
+using Android.Content;
+using Android.App;
+
+namespace MauiApp1HelloWorld;
+
+[BroadcastReceiver(Enabled = true, Exported = false)]
+public class AlarmReceiver : BroadcastReceiver
+{
+    public override void OnReceive(Context context, Intent intent)
+    {
+        // Wenn der Wecker klingelt, holen wir die MainPage in den Vordergrund
+        Intent i = new Intent(context, typeof(MainActivity));
+        i.AddFlags(ActivityFlags.NewTask | ActivityFlags.ClearTop | ActivityFlags.SingleTop);
+        context.StartActivity(i);
+
+        // Startet sofort die Shake-Challenge, sobald die App aufwacht
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            if (Shell.Current?.CurrentPage is MainPage mainPage)
+            {
+                mainPage.StartShakeChallenge();
+            }
+        });
+    }
+}
