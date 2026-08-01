c47edf893c55c22b16ea0140f010f78f33ca5dbf
Alex-DE-74 <infalanda@gmail.com>
2026-08-01 14:21:25 +0200

    Add toast message in AlarmReceiver on alarm start

--- Platforms/Android/AlarmReceiver.cs
+++ Platforms/Android/AlarmReceiver.cs
@@ -8,6 +8,11 @@ public class AlarmReceiver : BroadcastReceiver
 {
     public override void OnReceive(Context context, Intent intent)
     {
+        Android.Widget.Toast.MakeText(
+        context,
+        "AlarmReceiver gestartet",
+        Android.Widget.ToastLength.Long).Show();
+
         // Wenn der Wecker klingelt, holen wir die MainPage in den Vordergrund
         Intent i = new Intent(context, typeof(MainActivity));
         i.AddFlags(ActivityFlags.NewTask | ActivityFlags.ClearTop | ActivityFlags.SingleTop);
