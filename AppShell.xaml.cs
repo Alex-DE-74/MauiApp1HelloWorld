bd8d97d9132ff490ddecb82f904dad367306e3f0
Alex-DE-74 <infalanda@gmail.com>
2026-08-17 14:01:34 +0200

    Add AddExerciseCommand to AppShell
    Implemented AddExerciseCommand to add exercises from ExercisesPage.

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,12 +1,25 @@
 using KidJumpUp.Pages;
+using System.Windows.Input;
 
 namespace KidJumpUp;
 
 public partial class AppShell : Shell
 {
+
+    public ICommand AddExerciseCommand { get; }
+
     public AppShell()
     {
         InitializeComponent();
+        AddExerciseCommand = new Command(async () =>
+        {
+          if (Current?.CurrentPage is ExercisesPage page)
+          {
+            await page.AddExerciseAsync();
+          }
+        });
+
+        BindingContext = this;
 
         Routing.RegisterRoute(nameof(ExercisesPage), typeof(ExercisesPage));
         Routing.RegisterRoute(nameof(ExercisePlanPage), typeof(ExercisePlanPage));
