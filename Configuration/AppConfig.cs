0860c07bba516ecfc0fffef29e2a12b026a819a4
Alex-DE-74 <infalanda@gmail.com>
2026-08-20 00:40:03 +0200

    Create AppConfig.cs

---
+++ Configuration/AppConfig.cs
@@ -0,0 +1,10 @@
+namespace KidJumpUp.Configuration;
+
+public class AppConfig
+{
+    public double ExerciseFontSize { get; set; } = 20;
+
+    public double ExerciseRowVerticalPadding { get; set; } = 4;
+
+    public double ExerciseTargetFontSize { get; set; } = 20;
+}
