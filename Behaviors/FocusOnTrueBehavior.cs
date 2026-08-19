68958367c8aa76a33c202b76773765c3c7c076a5
Alex-DE-74 <infalanda@gmail.com>
2026-08-19 20:42:28 +0200

    Enhance BindingContext management in FocusOnTrueBehavior
    Refactor OnAttachedTo and OnDetachingFrom methods to handle BindingContext changes.

--- Behaviors/FocusOnTrueBehavior.cs
+++ Behaviors/FocusOnTrueBehavior.cs
@@ -20,16 +20,39 @@ public sealed class FocusOnTrueBehavior : Behavior<VisualElement>
 
     private VisualElement? AssociatedObject { get; set; }
 
-    protected override void OnAttachedTo(VisualElement bindable)
+    protected override void OnAttachedTo(
+        VisualElement bindable)
     {
         base.OnAttachedTo(bindable);
+
         AssociatedObject = bindable;
+
+        bindable.BindingContextChanged +=
+            OnBindingContextChanged;
+
+        BindingContext = bindable.BindingContext;
     }
 
-    protected override void OnDetachingFrom(VisualElement bindable)
+    protected override void OnDetachingFrom(
+        VisualElement bindable)
     {
+        bindable.BindingContextChanged -=
+            OnBindingContextChanged;
+
         AssociatedObject = null;
+
         base.OnDetachingFrom(bindable);
+    }
+
+    private void OnBindingContextChanged(
+        object? sender,
+        EventArgs e)
+    {
+        if (AssociatedObject == null)
+            return;
+
+        BindingContext =
+            AssociatedObject.BindingContext;
     }
 
     private static void OnTriggerChanged(
