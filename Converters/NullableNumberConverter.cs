bfb15f98127eab8cb4914bd1c1e45b05c7625d14
Alex-DE-74 <infalanda@gmail.com>
2026-08-19 22:34:01 +0200

    Refactor NullableNumberConverter methods for clarity

--- Converters/NullableNumberConverter.cs
+++ Converters/NullableNumberConverter.cs
@@ -1,23 +1,33 @@
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace KidJumpUp.Converters;
+
 public abstract class NullableNumberConverter<T> : IValueConverter
     where T : struct, IParsable<T>
 {
-    public object Convert(
+    public object? Convert(
         object? value,
         Type targetType,
         object? parameter,
         CultureInfo culture)
-        => value is T number
+    {
+        return value is T number
             ? number.ToString(null, culture)
             : string.Empty;
+    }
 
     public object? ConvertBack(
         object? value,
         Type targetType,
         object? parameter,
         CultureInfo culture)
-        => value is not string text || string.IsNullOrWhiteSpace(text)
-            ? null
-            : T.TryParse(text, culture, out var result)
-                ? result
-                : BindableProperty.UnsetValue;
+    {
+        if (value is not string text || string.IsNullOrWhiteSpace(text))
+            return null;
+
+        return T.TryParse(text, culture, out var result)
+            ? result
+            : BindableProperty.UnsetValue;
+    }
 }
