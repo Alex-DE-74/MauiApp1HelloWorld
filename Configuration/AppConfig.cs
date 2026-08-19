653dbe62591a9ea068ca96f91c6a7d6028c88292
Alex-DE-74 <infalanda@gmail.com>
2026-08-20 00:55:01 +0200

    Remove ExerciseRowVerticalPadding and add ExerciseRowPadding

--- Configuration/AppConfig.cs
+++ Configuration/AppConfig.cs
@@ -1,10 +1,13 @@
+using Microsoft.Maui;
+
 namespace KidJumpUp.Configuration;
 
 public class AppConfig
 {
     public double ExerciseFontSize { get; set; } = 20;
 
-    public double ExerciseRowVerticalPadding { get; set; } = 4;
-
     public double ExerciseTargetFontSize { get; set; } = 20;
+
+    public Thickness ExerciseRowPadding { get; set; } =
+        new(0, 4);
 }
