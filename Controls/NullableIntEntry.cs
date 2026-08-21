0c729847e27d8ed4d81146104f47c24de5a81e43
Alex-DE-74 <infalanda@gmail.com>
2026-08-21 03:19:02 +0200

    Implement static constructor for NullableIntEntry
    Add static constructor to NullableIntEntry for platform-specific mapping.

--- Controls/NullableIntEntry.cs
+++ Controls/NullableIntEntry.cs
@@ -1,3 +1,10 @@
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+
+#if ANDROID
+using AndroidX.AppCompat.Widget;
+#endif
+
 using KidJumpUp.Converters;
 
 namespace KidJumpUp.Controls;
@@ -5,4 +12,27 @@ namespace KidJumpUp.Controls;
 public class NullableIntEntry
     : NullableNumericEntry<int, NullableIntConverter>
 {
+
+    // Statischer Konstruktor läuft einmalig beim ersten Laden der Klasse
+    static NullableIntEntry()
+    {
+        // Überschreibt/Erweitert das Mapping global für diesen Typ, 
+        // ohne dass du jemals die MauiProgram.cs anfassen musst!
+        EntryHandler.Mapper.AppendToMapping(nameof(NullableIntEntry), (handler, view) =>
+        {
+            if (view is NullableIntEntry)
+            {
+                #if ANDROID
+                if (handler.PlatformView is AppCompatEditText nativeEditText)
+                {
+                    nativeEditText.Hint = view.Placeholder;
+                    if (view.PlaceholderColor != Colors.Transparent)
+                    {
+                        nativeEditText.SetHintTextColor(view.PlaceholderColor.ToPlatform());
+                    }
+                }
+                #endif
+            }
+        });
+    }
 }
